b392472730ed419509691091c24c4d1bd9aaa982
Thien Tri <thientri1146@gmail.com>
2025-11-08 15:49:36 +0700

    Demo lab2

---
+++ Lab2/Demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, nhan, chia;
+    printf("Nhap a va b:");
+    scanf("%f %f", &a, &b);
+    printf("Xuat ra 2 so thuc: %f %f", a, b);
+    nhan = a * b;
+    chia = a / b;
+    printf("\nNhan 2 so thuc:%.2f", nhan);
+    printf("\nChia 2 so thuc:%.2f", chia);
+    return 0;
+}
